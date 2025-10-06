a912835a23aed08a54601984cdee56ca62ba38f0
Nikita_Pl <bruuix@gmail.com>
2025-10-06 16:43:20 +0700

    Added css styles for needed button position

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -11,8 +11,12 @@ const {
   calculator__screen,
   calculator__operation,
   calculator__result,
+  container_top,
   container,
   container_numbers,
+  container_operations,
+  button_long,
+  button_operation,
 } = calculatorStyels;
 
 function Calculator() {
@@ -23,7 +27,7 @@ function Calculator() {
           <p className={calculator__operation}>6000/2</p>
           <h1 className={calculator__result}>=12546</h1>
         </div>
-        <div className={container}>
+        <div className={container_top}>
           <button className={`${button} ${button_math}`}>e</button>
           <button className={`${button} ${button_math}`}>sin</button>
           <button className={`${button} ${button_math}`}>cos</button>
@@ -32,24 +36,36 @@ function Calculator() {
           <button className={button}>
             <img className={icon} src={icon_delete} alt="delete-icon" />
           </button>
-          <button className={button}>/</button>
-          <button className={button}>*</button>
-          <div className={container_numbers}>
-            <button className={`${button}`}>7</button>
-            <button className={`${button}`}>8</button>
-            <button className={`${button}`}>9</button>
-            <button className={`${button}`}>4</button>
-            <button className={`${button}`}>5</button>
-            <button className={`${button}`}>6</button>
-            <button className={`${button}`}>1</button>
-            <button className={`${button}`}>2</button>
-            <button className={`${button}`}>3</button>
-            <button className={`${button} ${button_number}`}>0</button>
-            <button className={`${button}`}>.</button>
+          <button className={`${button} ${button_operation}`}>/</button>
+          <button className={`${button} ${button_operation}`}>*</button>
+          <div className={container}>
+            <div className={container_numbers}>
+              <button className={`${button}`}>7</button>
+              <button className={`${button}`}>8</button>
+              <button className={`${button}`}>9</button>
+              <button className={`${button}`}>4</button>
+              <button className={`${button}`}>5</button>
+              <button className={`${button}`}>6</button>
+              <button className={`${button}`}>1</button>
+              <button className={`${button}`}>2</button>
+              <button className={`${button}`}>3</button>
+              <button className={`${button} ${button_number}`}>0</button>
+              <button className={`${button}`}>.</button>
+            </div>
+            <div className={container_operations}>
+              <button className={`${button} ${button_operation}`}>-</button>
+              <button
+                className={`${button} ${button_operation} ${button_long}`}
+              >
+                +
+              </button>
+              <button
+                className={`${button} ${button_operation} ${button_long}`}
+              >
+                =
+              </button>
+            </div>
           </div>
-          <button className={button}>-</button>
-          <button className={button}>+</button>
-          <button className={button}>=</button>
         </div>
       </div>
     </>
